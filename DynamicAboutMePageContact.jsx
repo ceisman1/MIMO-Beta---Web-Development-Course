2b96a59721c9841c29c5f86b687b98c0b862688f
Christine Eisman <89891365+ceisman1@users.noreply.github.com>
2023-08-01 16:26:10 -0400

    Dynamic About Me Contact Page

---
+++ DynamicAboutMePageContact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = (props) => {
+  return (
+    <div>
+      <h3>{props.greeting}</h3>
+      <button>{props.callToAction}</button>
+    </div>
+  );
+};
+export default Contact;
